2bf20df1e5e90135c7a2253b0ec1aa3b6809985f
jwu23 <jasonwu1623@gmail.com>
2020-10-08 00:13:11 -0700

    Completed es6 pattern errors

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.inCount = 0;
+    this.outCount = 0;
+    this.val;
   }
 
+  enqueue(value) {
+    this[this.inCount] = value;
+    this.inCount++;
+    this.count++;
+  }
+
+  dequeue() {
+    this.val = this[this.outCount];
+    delete this[this.outCount];
+    this.outCount++;
+    this.count--;
+    return this.val;
+  }
+
+  size() {
+    if (this.count < 0) {
+      return 0;
+    }
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.val;
+  }
+
+  push(value) {
+    this[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    this.val = this[this.count - 1];
+    delete this[this.count - 1];
+    this.count--;
+    return this.val;
+  }
+
+  size() {
+    if (this.count < 0) {
+      return 0;
+    }
+    return this.count;
   }
 
 }
