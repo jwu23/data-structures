5b52a66cf9a3d46ac5de373e8391e752e80b4de2
jwu23 <jasonwu1623@gmail.com>
2020-10-07 23:31:35 -0700

    Completed all errors for pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.inCount = 0;
+  this.outCount = 0;
+  this.val;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.inCount] = value;
+  this.inCount++;
+  this.count++;
+}
 
+Queue.prototype.dequeue = function() {
+  this.val = this[this.outCount];
+  delete this[this.outCount];
+  this.outCount++;
+  this.count--;
+  return this.val;
+}
+
+Queue.prototype.size = function() {
+  if (this.count < 0) {
+    return 0;
+  }
+  return this.count;
+}
