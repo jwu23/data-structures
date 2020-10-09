16ca93a7c44ff4addfdf17262cbc65d1bc0659c0
frmendez21 <frmendez209@gmail.com>
2020-10-08 17:10:49 -0700

    Linked List Complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // if tail is null     // tail is 4
+    // else if its not null
+    // move the tail over and create a new tail
+    if (list.head === null) {
+      list.head = Node(value);
+    }
+    list.tail = Node(value);
+
   };
 
+  // adding 4 to the tail
+  // expecting linked.removeHead()
+
   list.removeHead = function() {
+    // when we remove 4 reset the head to 5
+    // { head:5}
+    var removed = list.head;
+    delete list.head;
+    list.head = list.tail;
+    return removed.value;
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (target === list[key].value) {
+        return true;
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
