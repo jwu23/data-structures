a428280d7b2df6101a9a42c5cdfbc3a26c66784b
frmendez21 <frmendez209@gmail.com>
2020-10-08 21:01:18 -0700

    Tree Complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,13 @@
 var Tree = function(value) {
   var newTree = {};
+  // newTree.index = 0;
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // children should point to the next node on the list
+  newTree.children = []; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +15,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var grandchildren = new Tree(value);
+  this.children.push(grandchildren);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
+
 };
 
 
